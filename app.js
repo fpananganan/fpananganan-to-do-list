d53d36095d3fef5eb7b03d85600d3560700f28e2
Rico Pananganan <fpananganan@gmail.com>
2018-11-28 18:02:17 -0800

    checkpoint 6: completed

--- app.js
+++ app.js
@@ -14,32 +14,31 @@ function onReady() {
 
     renderTheUI();
   }
-}
 
-function renderTheUI() {
-  const toDoList = document.getElementById('toDoList');
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
 
-  toDoList.textContent = '';
+    toDoList.textContent = '';
 
-  toDos.forEach(function(toDo) {
-    const newLi = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
+    toDos.forEach(function(toDo) {
+      const nweLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
 
-    newLi.textContent = toDo.title;
+      newLi.textContent = toDo.title;
 
-    toDoList.appendChild(newLi);
-    newLi.appendChild(checkbox);
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
 
+  renderTheUI();
 }
-
-addToDoForm.addEventListener('submit', event => {
-  event.preventDefault();
-  createNewToDo();
-});
-
-renderTheUI();
 
 window.onload = function() {
   onReady();
