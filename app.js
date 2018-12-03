c9cfff92d8974bbe682565bcdf10296c8efcf4e9
Rico Pananganan <fpananganan@gmail.com>
2018-12-03 11:07:09 -0800

    Assignment 5-revised

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
 function onReady() {
-  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-
-  function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
-
-  function renderTheUI() {
-    const toDoList = document.getElementById('toDoList');
-
-    toDoList.textContent = '';
-
-    toDos.forEach(function(toDo) {
-      const nweLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
-
-      newLi.textContent = toDo.title;
-
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-    });
-  }
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-    createNewToDo();
-  });
 
-  renderTheUI();
+    // get the newToDoText
+    let title = newToDoText.value;
+
+    // create a new Li
+    let newLi = document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    // create a delete button
+    let deleteBut = document.createElement('button');
+
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+
+    // set the button type to delete
+    deleteBut.textContent = "delete";
+
+    // set the title
+    newLi.textContent = title;
+
+    // attach the checkbox to the Li
+    newLi.appendChild(checkbox);
+
+    // attach the Li to the ul
+    toDoList.appendChild(newLi);
+
+    // delete the Li from the ul
+    toDoList.appendChild(deleteBut);
+
+    //empty the input
+    newToDoText.value = '';
+
+    deleteBut.addEventListener('click', event => {
+      toDoList.removeChild(newLi);
+      toDoList.removeChild(deleteBut);
+    });
+
+  });
 }
 
 window.onload = function() {
+  alert("The window has loaded!");
   onReady();
 };
